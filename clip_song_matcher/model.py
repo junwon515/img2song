15ae2926400b1d8313dffa3129b5847a526edc33
Junwon515 <wnsdnjs7731@gmail.com>
2025-06-16 05:35:23 +0900

    feat: ProjectionHead 클래스 Multi-Layer Perceptron 구조로 변경

--- clip_song_matcher/model.py
+++ clip_song_matcher/model.py
@@ -1,10 +1,17 @@
 import torch.nn as nn
+from clip_song_matcher.config import INPUT_DIM, PROJ_DIM
 
 
 class ProjectionHead(nn.Module):
-    def __init__(self, in_dim: int = 512, out_dim: int = 512):
+    def __init__(self, in_dim: int = INPUT_DIM, out_dim: int = PROJ_DIM):
         super().__init__()
-        self.linear = nn.Linear(in_dim, out_dim)
+        hidden_dim = 2048
+        
+        self.mlp = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, out_dim)
+        )
 
     def forward(self, x):
-        return self.linear(x)
+        return self.mlp(x)
